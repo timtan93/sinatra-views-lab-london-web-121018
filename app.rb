9c1ae03aef1a9a6a9c256f6f41371903851a908d
timtan93 <timtan93@gmail.com>
2019-01-07 21:48:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
+require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+	end
+
+	get '/date' do
+		erb :date
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/hello' do
+		erb :hello
 	end
 
 
